863155795734dfff60cfb79e7a1a5f3b4177b688
Andrey Oleynik <aoleynik@chegg.com>
2020-02-08 08:20:43 +0200

    Add greed styling

--- lib/widgets/categories_screen.dart
+++ lib/widgets/categories_screen.dart
@@ -10,12 +10,14 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Flutter meal"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map((catData) => CategoryItem(catData.title, catData.color))
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
             mainAxisSpacing: 20),
       ),
     );
