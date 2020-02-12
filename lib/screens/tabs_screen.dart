92c72bffbd8f38b12a3e14fe8b513f593bd6035a
Andrey Oleynik <aoleynik@chegg.com>
2020-02-12 18:24:53 +0200

    Refactor tabs screen to bottom navigation

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:p7_meal_app/screens/categories_screen.dart';
-
-import 'favorite_screen.dart';
+import 'package:p7_meal_app/screens/favorite_screen.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -9,31 +8,50 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<TabData> _pages = [
+    TabData(CategoriesScreen(), "Categories"),
+    TabData(FavoriteScreen(), "Favorites"),
+  ];
+  int _selectedPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Meals"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: "Favorites",
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(children: <Widget>[
-          CategoriesScreen(),
-          FavoriteScreen()
-        ],),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex].title),
+      ),
+      body: _pages[_selectedPageIndex].screen,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        //type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              title: Text("Categories")),
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              title: Text("Favorites")),
+        ],
       ),
     );
   }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+}
+
+class TabData {
+  final Widget screen;
+  final String title;
+
+  TabData(this.screen, this.title);
 }
